097fa765aed15f4f2410c9c5ade9b2fd3b715e56
AdrienENSEA <adrien-sermet@live.fr>
2019-10-22 08:12:13 +0200

    Changement dia2code

--- extern/dia2code/src/generate_code_cpp.c
+++ extern/dia2code/src/generate_code_cpp.c
@@ -786,7 +786,14 @@ void print_include_stdlib(struct stdlib_includes* si,char* name) {
        if (!si->sfmlGraphics 
        && (strstr(name,"sf::RenderWindow")
        ||  strstr(name,"sf::VertexArray")
-       ||  strstr(name,"sf::Texture"))) {
+       ||  strstr(name,"sf::Texture")
+       ||  strstr(name,"sf::Text")
+       ||  strstr(name,"sf::Drawable")
+       ||  strstr(name,"sf::Transformable")
+       ||  strstr(name,"sf::Texture")
+       ||  strstr(name,"sf::Font")
+       ||  strstr(name,"sf::Sprite")
+       ||  strstr(name,"sf::RectangleShape"))) {
            print ("#include <SFML/Graphics.hpp>\n");
            si->sfmlGraphics = 1;
        }       
--- src/client/render/Scene.cpp
+++ src/client/render/Scene.cpp
@@ -3,20 +3,13 @@
 namespace render {
 
 Scene::Scene () {
-    uint height = 512;
+    height = 512;
+    length = 512;
+    sf::RenderWindow window(sf::VideoMode(length, height), "Tilemap");
 }
 
-Attack::Attack (IdAttack id_attack, std::string name, std::string descriptive_attack, Type type, StatsAttack stats_attack, ModifStatsPokemon stats_modif) {
-	this->id_attack = id_attack;
-	this->name = name;
-	this->descriptive_attack = descriptive_attack;
-	this->type = type;
-	this->stats_attack = stats_attack;
-	this->stats_modif = stats_modif;
-}
-
-void Attack::Scene (ModifStatsPokemon stats_modif) {
-	this->stats_modif = stats_modif;
+void Scene::draw () {
+    
 }
 
 }
