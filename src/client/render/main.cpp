2bcc60011c7dafae42e3be716dfcf93f9b93e31a
AdrienENSEA <adrien-sermet@live.fr>
2019-10-22 08:40:28 +0200

    exec render ok

---
+++ src/client/render/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
+#include <SFML/Graphics.hpp>
+
+void testSFML() {
+    sf::Texture texture;
+}
+
+// Fin test SFML
+
+#include "../render.h"
+#include <string.h>
+
+using namespace std;
+using namespace render;
+
+int main(int argc, char* argv[])
+{
+    //Exemple exemple;
+    //exemple.setX(53);
+	
+    Scene scene();
+    return 0;
+}
